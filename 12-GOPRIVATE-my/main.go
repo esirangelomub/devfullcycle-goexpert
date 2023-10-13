87c9e9924d7ea9d7f0aad28ca41046a4ec8b5957
Eduardo Sirangelo <eduardo.sirangelo@gmail.com>
2023-10-13 14:13:00 -0300

    feat(12-GOPRIVATE-my): Implement and Document GOPRIVATE Usage
    // Description
    This commit introduces the GOPRIVATE concept, providing a thorough understanding and practical implementation details. It demonstrates how to manage and utilize private Go modules by interacting with private repositories on GitHub and Bitbucket. This chapter furnishes the codebase with examples, documentation, and guides on how to use GOPRIVATE to keep specific modules private, manage dependencies securely, and ensure a smooth development workflow when dealing with private modules.
    
    // Changes
    The major changes incorporated in this chapter include:
    - Implementing the usage of GOPRIVATE to avoid leaking proprietary dependencies.
    - Crafting practical examples using both GitHub and Bitbucket to demonstrate secure dependency management.
    - Documentation that gives insights on managing private modules, dependencies, and access control for the same.
    - Additional tips and best practices for using private modules securely and efficiently in Go development.
    
    // Note
    This chapter is crucial for developers working with private and proprietary modules, ensuring secure and efficient dependency management and safeguarding the proprietary code from unauthorized access.

--- 12-GOPRIVATE-my/main.go
+++ 12-GOPRIVATE-my/main.go
@@ -5,6 +5,17 @@ import (
 	"github.com/esirangelomub/fcutils-secret/pkg/events"
 )
 
+/*
+Some commands to run:
+- go mod init github.com/esirangelomub/fcutils-secret
+- go mod tidy
+Environment configuration:
+- .netrc file in the home directory
+  - github.com	<username>	<token>
+  - api.bitbucket.org	<username>	<token>
+
+- export GOPRIVATE="github.com/esirangelomub/*"
+*/
 func main() {
 	ed := events.NewEventDispatcher()
 	fmt.Println(ed)
